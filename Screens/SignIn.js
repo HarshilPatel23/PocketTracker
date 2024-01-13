d711ca9728a85e89924900fbe4fa70c965f4218f
Harshil Patel <harshilpatelb202@gmail.com>
2024-01-12 19:28:47 -0700

    firebase setup

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,8 +1,25 @@
 import { StyleSheet, View, StatusBar, TouchableOpacity } from 'react-native'
 import { Button, TextInput,Text } from 'react-native-paper';
-import React from 'react'
+import React, { useState } from 'react';
+import { auth1 } from '../firebaseUtil';
 
 const SignIn = ({navigation}) => {
+    const [userEmail,setUserEmail]=useState('')
+    const [userPassword,setUserPassword]=useState('')
+    const [loading,setloading]=useState(false)
+    // const auth=auth1()
+    // const signIn=async ()=>{
+    //     setloading(true);
+    //     try
+    //     {
+    //         const response=await auth.signInWithEmailAndPassword(auth,userEmail,userPassword);
+    //         console.log(response);
+    //     } catch(error){
+    //         console.log(error)
+    //     } finally{
+    //         setloading(false);
+    //     }
+    // }
   return (
     <View style={styles.container}>
     <StatusBar style="light" />
@@ -23,7 +40,7 @@ const SignIn = ({navigation}) => {
       <Button style={styles.button} mode="contained" onPress={() => console.log('Pressed')}>
         Sign In
     </Button>
-    <Button style={styles.button} icon="google" mode="contained" onPress={() => console.log('Pressed')}>
+    <Button style={styles.button} icon="google" mode="contained" >
         Sign In with Google
     </Button>
     
